a6c01aa1f1ad9ae27f3fd1c910e8489b6595ff5b
Tom McClintock <tmcclintock89@gmail.com>
2017-04-18 19:53:39 -0700

    Renamed some files. Started working on quicksort.

---
+++ sorting/quicksort.py
@@ -0,0 +1,13 @@
+"""
+My implementation of the quicksort algorithm.
+"""
+from swap import *
+import numpy as np
+
+def quicksort(array):
+    #Base case
+    if len(array) <= 0: return array
+
+
+if __name__ == "__main__":
+    data = np.loadtxt("data.txt")
